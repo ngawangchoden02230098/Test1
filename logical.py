650b2000635aa50112ff147612009fcd47ce7baf
Ngawang Choden <02230098.cst@rub.edu.bt>
2024-02-26 14:59:58 +0545

    Adding logical.py file

---
+++ logical.py
@@ -0,0 +1,23 @@
+#Example 1
+x = 5
+print(x > 3 and x < 10) #True beczuse both conditions are True
+
+ #Example 2 
+y = 12
+print(y > 10 and y % 5 == 0) # False because the second conditon is Talse
+
+#Example 1
+a = 5
+print(x < 3 or x > 10) #False because both conditions are False
+
+#Example 2
+b = 12
+print(y < 10 or y % 2 == 0) # True because the second condition is True
+
+#Example 1
+c = 5
+print(not(x > 3 or x > 10)) # False because the condition inisde the not is True
+#Example 2
+d = 12
+print(not(y < 10 or y % 5 == 0)) # True because the comdition inside the not is False
+
