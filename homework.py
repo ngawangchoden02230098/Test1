1e4f7da2ab12747465585fdf10da165e58230ebf
Ngawang Choden <02230098.cst@rub.edu.bt>
2024-02-27 13:29:04 +0545

    Adding homework.py file

--- homework.py
+++ homework.py
@@ -1,8 +1,6 @@
 age = int(input("Enter your age: "))
-student = input("Are you a student?: ")
-if age <= 12:
-    print("You are eligible for a discount on the movie ticket")
-elif 13<= age <= 18 and student == "yes" :
+student = input("Are you a student? (yes/no): ").lower()
+if age <= 12 or (13<= age <= 18 and student == "yes" ):
     print("You are eligible for a discount on the movie ticket")
 else:
     print("You are not eligible for a discount on the movie ticket")
