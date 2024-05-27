9016664968b123a612f64d814be9517d40825176
Ngawang Choden <02230098.cst@rub.edu.bt>
2024-05-27 12:37:53 +0530

    Adding classwork.py file

---
+++ classwork.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if (age <= 12 ):
+    print("You are eligible for a discount on the movie ticket.")
+std = input("Are you student? (yes/no): ")
+
+if (13<= age <= 18 and std == "yes"):
+    print("You are eligible for a discount on the movie ticket.")
+else:
+        print("You are not eligible for a discount on the movie ticket.")
